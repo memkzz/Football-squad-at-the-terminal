601f62f2cd5443f3457f87b8751dc35c708e26b0
Emerson Ferreira <sfr.emerson@gmail.com>
2025-01-14 11:39:13 -0300

    Init

---
+++ src/domain/Player.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class Player {
+    private String name;
+    private int age;
+    private float height;
+    private String position;
+    private String foot;
+    private float marketValue;
+    private String status;
+    private String endOfContract;
+
+}
